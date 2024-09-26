23e5a3bd071ab4c4d378b27bfe2f3523336dd90f
nhatnguyen061 <nhatkarit000@gmail.com>
2024-09-26 13:35:59 +0700

    RE-added file after clean history

---
+++ Assets/Scripts/DamagePlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            //tìm đến đối tượng có lớp playerheath để xử lý sát thương với singletion
+            PlayerHealthController.instance.DealDamage();
+        }
+          
+    }
+}
