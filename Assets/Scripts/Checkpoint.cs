cacc4fbae9493c28c64af841675d256d5505e1bd
nhatnguyen061 <nhatkarit000@gmail.com>
2024-10-15 23:15:48 +0700

    Done Checkpoint
    Done Checkpoint

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public SpriteRenderer theSR;
+    public Sprite cpOn, cpOff;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //xác định chuyển màu checkpoint
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            CheckpointController.instance.DeactivateCheckpoints();
+            theSR.sprite = cpOn;
+
+            CheckpointController.instance.SetSpawnPoint(transform.position);
+        }
+    }
+
+    public void ResetCheckpoint()
+    {
+        theSR.sprite = cpOff;
+    }
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public static LevelManager instance;
+
+    //khi chết thì chờ thời gian để hồi sinh
+    public float waitToRespawn;
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RespawnPlayer()
+    {
+        //tao vong lap hoi sinh trong vong bao nhieu giay
+        StartCoroutine(RespawnCo());
+
+    }
+    //tạo một vòng lặp cho việc hồi sinh nằm ngoài vòng lặp ụpdate
+    private IEnumerator RespawnCo()
+    {
+        //player duoc gan script PlayerController
+        PlayerController.instance.gameObject.SetActive(false);
+        //khi thực hiện hàm trên thì chờ một thời gian với hàm yield
+        yield return new WaitForSeconds(waitToRespawn);
+
+        PlayerController.instance.gameObject.SetActive(true);
+        PlayerController.instance.transform.position = CheckpointController.instance.spawnPoint;
+
+        //set lại máu lại ban đầu cho player
+        PlayerHealthController.instance.currentHealth = PlayerHealthController.instance.maxHealth;
+        UIController.instance.UpdateHealthDisplay();
+
+    }
+
+}
